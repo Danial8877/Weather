1432c87f84ba3ec8950005578dad370ce28b771e
Danial Jamshidi <danialjamshidi8877@gmail.com>
2025-06-29 16:56:58 +0330

    first commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const storage = new Storage();
+const weatherLocation = storage.getLocationData();
+
+const weather = new Weather(weatherLocation);
+const ui = new UI();
+
+document.addEventListener('DOMContentLoaded', getWeather);
+function getWeather() {
+    ui.loading(true);
+    weather.getWeather()
+        .then((result) => {
+            // console.log(result);
+            ui.paint(result, weather.location);
+            ui.loading(false);
+        }).catch((err) => {
+            console.log(err);
+            ui.loading(false);
+        });
+}
+
+document.getElementById('w-change-btn').addEventListener('click', changeLocation);
+const locationModal = new bootstrap.Modal('#locationModal');
+
+function changeLocation() {
+    const city = document.getElementById('city').value;
+    weather.changeLocation(city);
+    storage.setLocationData(city);
+    locationModal.hide();
+
+    getWeather();
+}
